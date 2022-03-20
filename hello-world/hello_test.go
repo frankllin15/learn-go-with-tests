d4c5c6d57fe73428e222cbafec641c5de0e01d80
Frankllin <frankllin15@hotmail.com>
2022-03-20 14:22:13 -0300

    add: hello-world module and tests

---
+++ hello-world/hello_test.go
@@ -0,0 +1,27 @@
+package helloworld
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	assertCorrectMessage := func(t testing.TB, got string, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
+	t.Run("sayng hello to people", func(t *testing.T) {
+		got := Hello("Frank")
+		want := "Hello, Frank"
+
+		assertCorrectMessage(t, got, want)
+
+	})
+	t.Run("say 'Hello, World' when an empty string is suppied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+
+	})
+}
