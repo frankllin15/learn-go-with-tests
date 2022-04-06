b107046a1e0642fa1efa4d04ac746656b3801a9d
Frankllin <frankllin15@hotmail.com>
2022-04-06 17:53:05 -0300

    refact: create helper function test and deacouple

--- structs/geometry.go
+++ structs/geometry.go
@@ -1,15 +1,33 @@
 package structs
 
+import "math"
+
 type Rectangle struct {
-	width  float64
-	height float64
+	Width  float64
+	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+type Shape interface {
+	Area() float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
 
 func Perimeter(rectangle Rectangle) float64 {
 
-	return 2 * (rectangle.width + rectangle.height)
+	return 2 * (rectangle.Width + rectangle.Height)
 }
 
 func Area(rectangle Rectangle) float64 {
-	return rectangle.width * rectangle.height
+	return rectangle.Width * rectangle.Height
 }
