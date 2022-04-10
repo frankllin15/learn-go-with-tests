a5bc56ab840f892efdd0e6aee1303329a8dd2044
Frankllin <frankllin15@hotmail.com>
2022-04-10 16:40:20 -0300

    dependency injecton example

---
+++ dependency-injection/di._test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
+	Greet(&buffer, "Frank")
+
+	got := buffer.String()
+	want := "Hello, Frank"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+
+}
